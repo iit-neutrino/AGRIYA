6d4aabdc77bc7d4c3bdc90f0f9114feff18d23d6
Dragonfire3900 <tangentialcubic@gmail.com>
2023-03-28 12:55:31 -0500

    Got the mask system for erroring out with repeats

--- src/JSONSerializer.cc
+++ src/JSONSerializer.cc
@@ -1,6 +1,9 @@
 #include <map>
+#include <vector>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <format>
 
 #include "TMatrix.h"
 
@@ -10,26 +13,63 @@ using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
     std::string files[] = {
-        "./inputs/test.json",
-        "./inputs/test2.json"
+        "../inputs/test.json",
+        "../inputs/test2.json",
+        "../inputs/test3.json"
     };
 
     std::map<std::string, std::vector<double>> map = {};
-    std::string expNames[sizeof(files) / sizeof(files[0])] = {};
+    std::vector<std::string> expNames(sizeof(files) / sizeof(files[0]));
 
     TMatrix covSyst = TMatrix(sizeof(files) / sizeof(files[0]), sizeof(files) / sizeof(files[0]));
+    TMatrix mask = TMatrix(sizeof(files) / sizeof(files[0]), sizeof(files) / sizeof(files[0])); //If the value has already been modified
     covSyst.Zero();
+    mask.Zero();
+    
     TMatrix covStat = TMatrix(sizeof(files) / sizeof(files[0]), sizeof(files) / sizeof(files[0]));
     covStat.Zero();
 
     int idx = 0;
+    int otherIdx, smallerIdx, largerIdx;
+
+
     for (auto file: files) {
         std::fstream f(file);
         json data = json::parse(f);
 
         expNames[idx] = data["expName"];
+        ++idx;
+    }
+
+    idx = 0;
+
+    for (auto file: files) {
+        std::fstream f(file);
+        json data = json::parse(f);
+
         covSyst(idx, idx) = data["IBDUncertainty"]["syst"];
         covStat(idx, idx) = data["IBDUncertainty"]["stat"];
+        mask(idx, idx) = 1;
+
+        for (auto& dep: data.at("Dependencies").items()) {
+            otherIdx = std::find(expNames.begin(), expNames.end(), dep.key()) - expNames.begin(); //Getting the index of the other experiment
+            if (otherIdx != (expNames.end() - expNames.begin())) {
+                smallerIdx = std::min(idx, otherIdx);
+                largerIdx = std::max(idx, otherIdx);
+
+                if (mask(smallerIdx, largerIdx) > 0) {
+                    std::string errorVal = "Tried to set already set value in the covariance matricies. Please check files: ";
+                    errorVal += files[smallerIdx] + ", and " + files[largerIdx];
+                    errorVal += " and make the dependencies unique.";
+                    throw std::runtime_error(errorVal);
+                }
+
+                mask(smallerIdx, largerIdx) = 1;
+                covSyst(smallerIdx, largerIdx) = dep.value().value("syst", 0.0);
+                covStat(smallerIdx, largerIdx) = dep.value().value("stat", 0.0);
+            }
+        }
+
         for (auto& el: data.at("fuels").items()) {
             std::cout << "Key: " << el.key() << ", item: " << el.value() << std::endl;
 
@@ -52,4 +92,7 @@ int main(int argc, char *argv[]) {
 
         std::cout << ")" << std::endl;
     }
+
+    covSyst.Print();
+    covStat.Print();
 }
